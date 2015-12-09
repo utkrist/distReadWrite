ac6862b46f359fc94c12eb41917bd89b7be33128
Utkrist Adhikari <utkrist@outlook.com>
2015-12-09 22:10:17 +0100

    Multi-threaded version in order to handle simultaneous requests

--- Python_version/distReadWrite.py
+++ Python_version/distReadWrite.py
@@ -1,64 +1,50 @@
+"""" TODO
+Synchronisation of join
+- Way to deal with inconsistency when two separate clusters try to join the the network simultaneously
+  or to put it into perspectve : what to do about the request to join from a cluster when a network update is in progress.
+
+Synchronisation of join
+""""
+
 import xmlrpclib, sys, argparse, threading
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 
-# Some constants
-IP = 0
-PORT = 1
+IP = 0, PORT = 1                      # Some constants
+my_ip = "localhost", my_port =  2000  # Network address of the current node
+my_neighbor = []                      # Store ip_addr and port of each neighbor
 
-# Network address of the current node
-my_ip = "localhost"
-my_port =  2000
+# Mix-in for threaded xmlrpc server
+class ThreadedXMLRPCServer(ThreadingMixIn, SimpleXMLRPCServer):pass
 
-# Store ip_addr and port of each neighbor
-my_neighbor = []
+class MethodLibrary:
+	# Updates the neighbor with the list received from node of another cluster
+	def update_neighbor(node_ip, node_port, neighbor_list):
+		global my_neighbor, my_ip, my_port
+		print "(", my_ip , "," , my_port, ")== Update ==>(", node_ip , "," , node_port, ")"
+		if (node_ip, node_port) not in my_neighbor:
+			return False
+		for n in neighbor_list:
+			my_neighbor.append(n)
 
-# Updates the neighbor with the list received from node of 
-# another cluster
-def update_neighbor(node_ip, node_port, neighbor_list):
-	global my_neighbor, my_ip, my_port
-	print "(", my_ip , "," , my_port, ")== Update ==>(", node_ip , "," , node_port, ")"
-	if (node_ip, node_port) not in my_neighbor:
-		return False
-	for n in neighbor_list:
-		my_neighbor.append(n)
+	def join(node_ip, node_port, neighbor_list):
+		global my_neighbor, my_ip, my_port
+		print "(",my_ip,",",my_port,")== Join ==>(",node_ip,",",node_port,")"
+		if (node_ip, node_port) in my_neighbor:
+			return False
+		my_old_neighbor = my_neighbor
+		neighbor_list.append((node_ip, node_port))
+		for my_n in my_neighbor:
+			# Proxy is the server in contact and client in the local context
+			proxy_addr = "http://"+node_ip+":"+str(node_port)+"/"
+			proxy  = xmlrpclib.ServerProxy(proxy_addr, allow_none=True)
+			proxy.update_neighbor(my_ip, my_port, neighbor_list)
+		my_neighbor.append((node_ip, node_port))
+		print my_neighbor
+		return ((my_ip, my_port), my_old_neighbor)
 
-def join(node_ip, node_port, neighbor_list):
-	global my_neighbor, my_ip, my_port
-	print "(",my_ip,",",my_port,")== Join ==>(",node_ip,",",node_port,")"
-	if (node_ip, node_port) in my_neighbor:
-		return False
-	my_old_neighbor = my_neighbor
-	neighbor_list.append((node_ip, node_port))
-	for my_n in my_neighbor:
-		# Proxy is the server in contact and client in the local context
-		proxy_addr = "http://"+node_ip+":"+str(node_port)+"/"
-		proxy  = xmlrpclib.ServerProxy(proxy_addr, allow_none=True)
-		proxy.update_neighbor(my_ip, my_port, neighbor_list)
-	my_neighbor.append((node_ip, node_port))
-	print my_neighbor
-	return ((my_ip, my_port), my_old_neighbor)
-
-def sign_out(node_ip, node_port):
-	global my_neighbor
-	my_neighbor.remove((node_ip, node_port))
-
-def usage():
-	print "Usage: distReadWrite --port (or-p) <PORT>"
-	sys.exit(0)
-
-def parse_args():
-	global my_port
-	parser = argparse.ArgumentParser(prog='distReadWrite', description='This program performs distributed read write in a logical network.')
-	parser.add_argument('port', help='port number for the program to listen to', nargs=1, type=int)
-	my_port = int(parser.parse_args().port[0])
-
-def run_server():
-	global my_neighbor, my_ip, my_port
-	server = SimpleXMLRPCServer(("localhost", my_port), allow_none=True)
-	server.register_function(join, "join")
-	server.register_function(update_neighbor, "update_neighbor")
-	server.register_function(sign_out, "sign_out")
-	server.serve_forever()
+	def sign_out(node_ip, node_port):
+		global my_neighbor
+		my_neighbor.remove((node_ip, node_port))
 
 def run_client():
 	global my_neighbor, my_ip, my_port
@@ -69,18 +55,43 @@ def run_client():
 		my_neighbor.append(peer_address)
 	print my_neighbor
 
+def run_server():
+	global my_neighbor, my_ip, my_port
+	server = ThreadedXMLRPCServer(("localhost", my_port), allow_none=True)
+	server.register_instance(MethodLibrary())
+	server.serve_forever()
+
 def main():
 	global my_port
-	server = threading.Thread(target=run_server)
-	server.setDaemon(True)
-	server.start()	
-	if my_port != 2000:
-		client = threading.Thread(target=run_client)
-		client.setDaemon(True)
-		client.start()
-		client.join()
-	server.join()
+	serverThread = threading.Thread(target=run_server)
+	serverThread.setDaemon(True)
+	serverThread.start()
+	# Make an interface for join and signup
+	#if my_port != 2000:
+	#	client = threading.Thread(target=run_client)
+	#	client.setDaemon(True)
+	#	client.start()
+	#	client.join()
+	serverThread.join()
+
+def parse_args():
+	global my_port
+	parser = argparse.ArgumentParser(prog='distReadWrite', description='This program performs distributed read write in a logical network.')
+	parser.add_argument('port', help='port number for the program to listen to', nargs=1, type=int)
+	my_port = int(parser.parse_args().port[0])
 	
 if __name__ == "__main__":
-	parse_args()
+	parse_args()	
 	main()
+
+
+
+
+
+
+
+
+
+
+
+
